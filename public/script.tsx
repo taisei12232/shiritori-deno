d9ca1d74d67f865014fb0b7e0fbd07785afc2251
kiseki-kiseki <kcanggua@gmail.com>
2022-07-10 13:06:04 +0900

    first commit

---
+++ public/script.tsx
@@ -0,0 +1,18 @@
+import React from "https://cdn.skypack.dev/react@17.0.2?dts";
+import ReactDOM from "https://cdn.skypack.dev/react-dom@17.0.2?dts";
+// import lib from "https://cdn.skypack.dev/lib";
+function App() {
+  return (
+    <div>
+      <h2>Hello from React!</h2>
+    </div>
+  );
+}
+
+function main() {
+  ReactDOM.render(<App />, document.querySelector("#main"));
+}
+
+addEventListener("DOMContentLoaded", () => {
+  main();
+});
